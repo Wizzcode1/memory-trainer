08b432178355ee80e4239d1def374646933fa015
Wizzcode1 <wizzcode1@gmail.com>
2025-04-04 14:23:46 +0200

    add input autofocus

--- src/app/memory-game/memory-game.component.ts
+++ src/app/memory-game/memory-game.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { StartMenuComponent } from '../start-menu/start-menu.component';
@@ -19,6 +25,8 @@ export class MemoryGameComponent implements OnInit {
   showNumbers = false;
   inputVisible = false;
 
+  @ViewChildren('inputBox') inputBoxes!: QueryList<ElementRef<HTMLInputElement>>;
+
   ngOnInit(): void {
   }
 
@@ -27,14 +35,18 @@ export class MemoryGameComponent implements OnInit {
     this.startRound();
   }
 
-  startRound() {
-    this.currentSequence = Array.from({ length: this.sequenceLength }, () => Math.floor(Math.random() * 10));
+  startRound(): void {
+    this.currentSequence = Array.from({ length: this.sequenceLength }, () =>
+      Math.floor(Math.random() * 10)
+    );
     this.showNumbers = true;
     this.inputVisible = false;
 
     setTimeout(() => {
       this.showNumbers = false;
       this.inputVisible = true;
+
+      setTimeout(() => this.focusFirstInput(), 0);
     }, 5000);
   }
 
@@ -53,6 +65,13 @@ export class MemoryGameComponent implements OnInit {
     const nextInput = currentInput.nextElementSibling as HTMLInputElement | null;
     if (nextInput && currentInput.value.length === 1) {
       nextInput.focus();
+    }
+  }
+
+  focusFirstInput(): void {
+    const firstInput = this.inputBoxes.first;
+    if (firstInput) {
+      firstInput.nativeElement.focus();
     }
   }
 
